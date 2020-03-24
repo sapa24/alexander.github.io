1b4b93bcec7aa42aa43fd37b1ee6f6c59fc39351
sapa24 <62625554+sapa24@users.noreply.github.com>
2020-03-25 01:26:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.header-slider').slick({
+      arrows: false,
+      vertical: true,
+      autoplay: 3000,
+      dots: true,
+      dotsClass: 'header-dots'
+	});
+
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').slideToggle();
+	})
+
+});
